92cff7979a39633c8fa27981a713f4c1a200ca4e
Andre Ferreira <andre.ferreira.alves@gmail.com>
2019-04-22 13:03:25 -0300

    controller

---
+++ client/js/app/controllers/NegociacaoController.js
@@ -0,0 +1,26 @@
+class NegociacaoController {
+
+    constructor() {
+        let $ = document.querySelector.bind(document);
+
+        this._inputData = $('#data');
+        this._inputQuantidade = $('#quantidade');
+        this._inputValor = $('#valor');
+    }
+
+    adiciona(event) {
+        event.preventDefault();
+
+        // let data = new Date(this._inputData.value.split('-'));
+        let data = new Date(this._inputData.value.replace(/-/g, ','));
+        console.log(data);
+
+        // let negociacao = new Negociacao(
+        //     this._inputData.value,
+        //     this._inputQuantidade.value,
+        //     this._inputValor.value
+        // );
+
+        // console.log(negociacao);
+    }
+}
